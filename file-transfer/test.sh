594078a1c2470d29af44263f32580e97506b620c
stevecool2020 <stephen.cool@colorado.edu>
2020-11-24 19:10:25 -0500

    Adding file transfer test scripts and files

---
+++ file-transfer/test.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo "Starting file transfer..."
+
+input_file=$1
+clock_ms=$2
+output_file="/var/tmp/IR_data.txt"
+./file-transfer-app ${input_file} ${clock_ms}
+
+diff ${input_file} ${output_file} > /dev/null
+if [ $? -ne 0 ]; then
+        echo "Differences found"
+        echo "Expected contents to match:"
+        cat ${input_file}
+        echo "But found contents:"
+        cat ${output_file}
+        echo "With differences"
+        diff -u ${input_file} ${output_file}
+        echo "Test complete with failure"
+	exit 1
+else
+	echo "Contents match. Success"
+fi
+
